e1ae1725ff060dafb8760251211dd410dcba1b90
tod-easton <tod.easton.fv6o@statefarm.com>
2021-11-15 13:19:21 -0700

    add route table association

--- main.tf
+++ main.tf
@@ -41,13 +41,18 @@ resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.vpc0.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
     Name = "rt-tf"
   }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet0.id
+  route_table_id = aws_route_table.rt.id
 }
 
 # variable "common_cidr_block" {
