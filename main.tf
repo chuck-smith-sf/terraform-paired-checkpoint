21c4fdef7faa2460fe4819c5dac1c0f0d8c37206
cjsmith33 <charleswsmith8@gmail.com>
2021-11-15 12:41:04 -0700

    added VPC and subnet

--- main.tf
+++ main.tf
@@ -11,3 +11,48 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Create a VPC
+resource "aws_vpc" "vpc0" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc-terraform"
+  }
+}
+
+resource "aws_subnet" "subnet0" {
+  vpc_id     = aws_vpc.vpc0.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "sn-tf"
+  }
+}
+
+# variable "common_cidr_block" {
+#   type    = string
+#   default = "10.0.30.0/24"
+# }
+
+
+# resource "aws_subnet" "subnet0" {
+#   vpc_id     = aws_vpc.vpc0.id
+#   cidr_block = var.common_cidr_block
+
+#   tags = {
+#     Name = "example-subnet"
+#   }
+# }
+
+# output "subnet0_id" {
+#   value = aws_subnet.subnet0.vpc_id
+# }
+
+# resource "aws_subnet" "subnet0" {
+#   vpc_id     = aws_vpc.vpc0.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "example-subnet"
+#   }
+# }
